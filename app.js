cd17612e2e8fe4c26b16d791a4fc26a02c053833
Jeevan Muthu <75674374+Jeevan2305@users.noreply.github.com>
2023-08-29 18:29:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,77 @@
-const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const lowerSet = "abcdefghijklmnopqrstuvwxyz"
-const numberSet = "1234567890"
-const symbolSet = "~!@#$%^&*()_+/"
-
-// selectors
-const passBox = document.getElementById("pass-box")
-const totalChar = document.getElementById("total-char")
-const upperInput = document.getElementById("upper-case")
-const lowerInput = document.getElementById("lower-case")
-const numberInput = document.getElementById("numbers")
-const symbolInput = document.getElementById("symbols")
-
-const getRandomData = (dataSet) => {
-    return dataSet[Math.floor(Math.random() * dataSet.length)]
-}
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboard = document.getElementById("clipboard");
 
 const generatePassword = (password = "") => {
-    if (upperInput.checked) {
-        password += getRandomData(upperSet)
-    }
-    if (lowerInput.checked) {
-        password += getRandomData(lowerSet)
-    }
-    if (numberInput.checked) {
-        password += getRandomData(numberSet)
-    }
-    if (symbolInput.checked) {
-        password += getRandomData(symbolSet)
-    }
-    if (password.length < totalChar.value) {
-        return generatePassword(password)
-    }
+  if (uppercaseEl.checked) {
+    password += getRandomUpper();
+  }
+  if (lowercaseEl.checked) {
+    password += getRandomLower();
+  }
+  if (numbersEl.checked) {
+    password += getRandomNumber();
+  }
+  if (symbolsEl.checked) {
+    password += getRandomSymbol();
+  }
+  if (password.length < lengthEl.value) {
+    return generatePassword(password);
+  }
 
-    passBox.innerText = truncateString(password, totalChar.value);
-}
+  resultEl.innerText = truncateString(password, lengthEl.value);
+};
 
-// generatePassword();
-
-document.getElementById("btn").addEventListener("click", () => {
-        generatePassword()
-    }
-)
-
+generateEl.addEventListener("click", () => {
+  generatePassword();
+});
 
 function truncateString(str, num) {
-    if (str.length > num) {
-        let subStr = str.substring(0, num);
-        return subStr;
-    } else {
-        return str;
-    }
+  if (str.length > num) {
+    let subStr = str.substring(0, num);
+    return subStr;
+  } else {
+    return str;
+  }
 }
+
+function getRandomUpper() {
+  const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return upperSet[Math.floor(Math.random() * upperSet.length)];
+}
+
+function getRandomLower() {
+  const lowerSet = "abcdefghijklmnopqrstuvwxyz";
+  return lowerSet[Math.floor(Math.random() * lowerSet.length)];
+}
+
+function getRandomNumber() {
+  const numberSet = "1234567890";
+  return numberSet[Math.floor(Math.random() * numberSet.length)];
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+clipboard.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard");
+});
